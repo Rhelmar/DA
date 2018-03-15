19b98ee8260151f5b36509725c7ec4f3f802603c
IRechar <IRechar@users.noreply.github.com>
2018-03-15 21:11:33 +0100

    Add files via upload

---
+++ Booking.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Accounting
+{
+    /// <summary>
+    /// Interaktionslogik für Booking.xaml
+    /// </summary>
+    public partial class Booking : Window
+    {
+        public Booking()
+        {
+            InitializeComponent();
+            string[] bs = new string[3]{"1","2","3" };
+            Target.ItemsSource = bs;
+            Payment.ItemsSource = bs;
+            amount.Text = "Enter Amount...";
+            name.Text = "Name of Recipient...";
+            description.Text = "Enter Description";
+        }
+
+        private void LostFocusTextBox(object sender, RoutedEventArgs e)
+        {
+            var textboxElement = sender as TextBox;
+            string placeholderText = Convert.ToString(textboxElement.Tag);
+            var controlElement = sender as TextBox;
+            if (String.IsNullOrWhiteSpace(controlElement.Text))
+            {
+                controlElement.Text = placeholderText;
+            }
+        }
+
+        private void GotFocusTextBox(object sender, RoutedEventArgs e)
+        {
+            var textboxElement = sender as TextBox;
+            string placeholderText = Convert.ToString(textboxElement.Tag);
+            if (textboxElement.Text != "" && textboxElement.Text != placeholderText)
+            {
+                return;
+            }
+            textboxElement.Text = "";
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
